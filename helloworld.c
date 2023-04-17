788ca61d6e7d17be95efbdcada28f54793fa6ece
Bricecy <brigidwambua97@gmail.com>
2023-04-17 22:11:46 +0300

    Hello world

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World! This is Geeksforgeeks.org ")
+		return 0;
+}#include <stdio.h>
+
+int main()
+{
+	printf("Hello World! This is Geeksforgeeks.org ")
+		return 0;
+}
